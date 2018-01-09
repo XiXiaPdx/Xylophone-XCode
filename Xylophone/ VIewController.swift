b21708f75241e6eeb7a00bc39cd563a01671d72a
Xi Xia <triplextension@gmail.com>
2018-01-09 11:20:35 -0800

    note1 wav is playing from all buttons

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -2,15 +2,20 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 27/01/2016.
+//  Skeleton Created by Angela Yu on 27/01/2016.
 //  Copyright © 2016 London App Brewery. All rights reserved.
 //
+//  Guts filled in by Xi Xia on January 2018
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    // Grab the path, make sure to add it to your project!
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,12 +23,18 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let note1Url =  Bundle.main.url(forResource: "note1", withExtension: "wav")!
         
-        
+        do {
+            player = try AVAudioPlayer(contentsOf: note1Url)
+            guard let player = player else {return}
+            player.prepareToPlay()
+            player.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
         
     }
     
-  
-
 }
 
